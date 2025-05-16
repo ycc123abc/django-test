d53d62e2f68dc8d916af54887d9b97f6d71f02c3
ycc123abc <yccycc257@gmail.com>
2025-05-16 17:03:02 +0800

    bog

--- app01/models.py
+++ app01/models.py
@@ -19,17 +19,48 @@ class User(AbstractUser):
 
 
 
+class Category(models.Model):
+    name = models.CharField('博客分类',max_length = 100)
+    is_visble=models.IntegerField(verbose_name="是否可见")
+    
+    class Meta:
+       verbose_name = '博客分类'
+       verbose_name_plural = verbose_name
+	
+    def __str__(self):
+        return self.name
+    
+
+class Blog(models.Model):
+    blog_name=models.CharField(max_length = 255,db_comment="名称")
+    blog_text=models.TextField(max_length = 255,db_comment="内容")
+    category_id=models.TextField(max_length = 255,db_comment="Categoryid")
+    
+    class Meta:
+       verbose_name = '博客分类'
+       verbose_name_plural = verbose_name
+	
+    def __str__(self):
+        return self.name
+
+
+
+
 # admin.site.register(User)
 
 
-class classroom(models.Model):
-    classroom_id = models.CharField(max_length=50, unique=True, verbose_name='教室编号')
-    classroom_name = models.CharField(max_length=50, unique=True, verbose_name='教室名称')
-    classroom_capacity = models.IntegerField(verbose_name='教室容量')
-    classroom_location = models.CharField(max_length=50, verbose_name='教室位置')
-    classroom_type = models.CharField(max_length=50, verbose_name='教室类型')
+# class classroom(models.Model):
+#     classroom_id = models.CharField(max_length=50, unique=True, verbose_name='教室编号')
+#     classroom_name = models.CharField(max_length=50, unique=True, verbose_name='教室名称')
+#     classroom_capacity = models.IntegerField(verbose_name='教室容量')
+#     classroom_location = models.CharField(max_length=50, verbose_name='教室位置')
+#     classroom_type = models.CharField(max_length=50, verbose_name='教室类型')
 
-    class Meta:
-        db_table = 'classroom'
-        verbose_name = '教室'
-        verbose_name_plural = verbose_name
+#     class Meta:
+#         db_table = 'classroom'
+#         verbose_name = '教室'
+#         verbose_name_plural = verbose_name
+
+
+
+
